492a87e6727fa26b022b1f93d65f4f4eb658a0e2
benny <42614064+Benny805038@users.noreply.github.com>
2020-01-10 22:50:15 +0800

    Update and rename 123 to 155_Min Stack_06170127.py

---
+++ Leetcode/155_Min Stack_06170127.py
@@ -0,0 +1,26 @@
+
+class MinStack:
+
+    def __init__(self):
+    
+        self.list=[]
+        self.len=0
+        
+
+    def push(self, x: int) -> None:
+        self.list.append(x)
+        self.len+=1
+
+    def pop(self) -> None:
+        if self.len!=0:
+            self.list.pop(self.len-1)
+            self.len-=1
+        else:
+            pass
+
+    def top(self) -> int:
+        return self.list[self.len-1]
+
+    def getMin(self) -> int:
+        return min(self.list)
+
